251dfd116114926d6674bcbe40c5a937c0c336f9
Nur Muhammad Ilham <85226223+nurmilham@users.noreply.github.com>
2022-04-05 12:06:19 +0700

    Write your first C++ program

--- 1_HelloWorld/main.cpp
+++ 1_HelloWorld/main.cpp
@@ -5,3 +5,11 @@ int main(){
     cout << "Hello World";
     return 0; 
 }
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+“Hello World” program is the first step to learning any programming language.
+1] #include: In C++, (#) signs are handled by a preprocessor which is a program invoked by the compiler. The #include advised the compiler to include a file and #include<iostream>, tells the compiler to include the standard <iostream> library which contains declarations of input/output library functions.
+2] using namespace std: The using namespace statement just means that in the scope it is present, make all the things under the std namespace available without having to prefix std:: before each of them.
+3] int main(): This line is utilized to declare a function named "main" which returns data of integer. 
+4] return 0: This statement is used to return a value from a function and indicates the finishing of a function.
+*/
