18a1ea8ff42ebde3b3ad06ac96c29f19dcff193d
Nur Muhammad Ilham <85226223+nurmilham@users.noreply.github.com>
2022-04-05 15:19:16 +0700

    Learn about some data variables in C++

--- 2_DataVariables/main.cpp
+++ 2_DataVariables/main.cpp
@@ -13,6 +13,7 @@ int main(){
     // A character literal is created by enclosing a single character inside single quotation marks. ('A' , 'B' , '1' , 'o', etc.)
     // In C++, we can create variables whose value cannot be changed using const in front of the data type.
     const char characterBloodType = 'A';
+    // ---------------------------------------------------------------------------------------------------------------------------------
     cout << "My name is " << characterName << ',' << " study at " << characterAcademy << endl; // endl is a manipulator or in simple terms a command. So when endl is encountered, the operating system will flush the output buffer and insert a new line. 
     cout << "I am " << characterAge << " years old.\n"; // \n is called Escape Sequences. Sometimes, it is necessary to use characters that cannot be typed or has special meaning in C++ programming. i'll give you the list in the end of this program. 
     cout << "I am " << characterHeight << " cm and " << characterHeight << " kg" << endl;
